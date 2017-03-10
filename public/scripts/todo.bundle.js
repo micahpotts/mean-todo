18a4bb0279cc20a76e93af8ee256f175ef0919cf
Micah Potts <mrs.micahpotts@gmail.com>
2017-03-09 19:48:50 -0500

    App completed, minus bug fix

---
+++ public/scripts/todo.bundle.js
@@ -0,0 +1,43 @@
+webpackJsonp([0],[
+/* 0 */,
+/* 1 */
+/***/ (function(module, exports, __webpack_require__) {
+
+"use strict";
+
+
+var angular = __webpack_require__(0);
+
+angular.module('todoListApp')
+.controller('mainCtrl', function($scope, dataService){
+
+  dataService.getTodos(function(response){
+    var todos = response.data.todos;
+    $scope.todos =  todos;
+    });
+
+  $scope.addTodo = function() {
+    $scope.todos.unshift({name: "This is a new todo.",
+                      completed: false});
+  };
+
+})
+
+
+/***/ }),
+/* 2 */,
+/* 3 */
+/***/ (function(module, exports, __webpack_require__) {
+
+"use strict";
+
+
+var angular = __webpack_require__(0);
+
+angular.module('todoListApp', []);
+
+__webpack_require__(1);
+
+
+/***/ })
+],[3]);
--- src/api/index.js
+++ src/api/index.js
@@ -1,17 +1,44 @@
 'use strict';
 
 var express = require('express');
-var todos = require('../../mock/todos.json');
+var Todo = require('../models/todo');
+//var todos = require('../../mock/todos.json');
 
 var router = express.Router();
 
 router.get('/todos', function(req, res){
-  res.json({todos: todos})
+  Todo.find({}, function(err, todos) {
+    if(err) {
+      //dosomething
+      return res.status(500).json({message: err.message});
+    }
+    res.json({todos: todos});
+  })
+});
+
+router.post('/todos', function(req, res) {
+  var todo = req.body;
+  Todo.create(todo, function(err, todo) {
+    if(err) {
+      return res.status(500).json({err: err.message});
+    }
+    res.json({'todo': todo, message: 'Todo Created'});
+  })
 })
 
-//TODO: Add POST route to create new entries
-
-//TODO: Add PUT route to update existing entries
+router.put('/todos/:id', function(req, res) {
+  var id = req.params.id;
+  var todo = req.body;
+  if(todo && todo._id !== id) {
+    return res.status(500).json({err: "IDs don't match!"})
+  }
+  Todo.findByIdAndUpdate(id, todo,{new:true}, function(err, todo) {
+    if(err) {
+      return res.status(500).json({err: err.message});
+    }
+    res.json({'todo': todo, message: 'Todo Updated'});
+  })
+})
 
 //TODO: Add DEETE route to delete entries
 
